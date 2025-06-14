dd59798cf65de7bcd1a0ecd2283d0bb4c3e7dfa1
HamzaIqbal555 <hi437587@gmail.com>
2025-06-15 00:41:59 +0300

    Implemented simple rag with data ingestion, transformation and storing word embedding into vector store

--- api/client.py
+++ api/client.py
@@ -3,17 +3,25 @@ import streamlit as st
 
 
 def getOpenAI_response(input_text):
-    response = requests.post('https://localhost:8000/essay/invoke',
+    response = requests.post('http://localhost:8000/essay/invoke',
                              json={'input' : {'topic':input_text}})
     return response.json()['output']['content']
 
 
-def getLLama_response(input_text):
-    response = requests.post('https://localhost:8000/poem/invoke',
-                             json={'input' : {'topic': input_text}})
+def getLLama_response(input_text1):
+    response = requests.post('http://localhost:8000/poem/invoke',
+                             json={'input' : {'topic': input_text1}})
     return response.json()['output']
 
 ## Streamlit framework
 
 st.title("Langchain Demo with API")
-st.text
+input_text = st.text_input("Write an essay on")
+input_text1 = st.text_input('Write poem on')
+
+if input_text:
+    st.write(getOpenAI_response(input_text))
+
+if input_text1:
+    st.write(getLLama_response(input_text1))
+
